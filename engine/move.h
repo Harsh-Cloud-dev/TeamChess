fdbf0f132dce056ce06d2ac46b28e663296ca690
Harsh Thalwal <harshthalwal@gmail.com>
2025-10-12 11:11:34 +0530

    engine

---
+++ engine/move.h
@@ -0,0 +1,9 @@
+struct Move
+{
+    int fromRow,fromCol;
+    int toRow,toCol;
+    char promotion;
+    Move(int fr,int fc,int tr,int tc,char promo = '.')
+        :fromRow(fr),fromCol(fc),toRow(tr),toCol(tc),promotion(promo){}
+};
+//uni_64 Win32,AR64 only 
