e7082ffd7a995f3e51d8368f16cd1cfe2305a0ad
Harsh Thalwal <harshthalwal@gmail.com>
2025-10-21 07:30:37 +0530

    Frontend New Update

---
+++ frontend/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('TopPage')!).render(
+  <StrictMode>
+
+  </StrictMode>
+)
+createRoot(document.getElementById('MiddlePage')!).render(
+  <StrictMode>
+
+  </StrictMode>
+)
+createRoot(document.getElementById('EndPage')!).render(
+  <StrictMode>
+
+  </StrictMode>
+)
